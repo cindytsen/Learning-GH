8d0d7af22cd5526ea7c1256b0c4692e7d6f51996
Cindy Tsen <ctsenstudio@gmail.com>
2022-06-09 19:03:37 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Cindy T on 2022-06-09.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
